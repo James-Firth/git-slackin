63c68618f032ec23faf49e6f91206b100e138b31
James Firth <james.firth@norimaconsulting.com>
2019-12-08 13:25:57 -0600

    Start of better logging

--- src/lib/slack/message.js
+++ src/lib/slack/message.js
@@ -2,6 +2,7 @@ const { WebClient } = require('@slack/web-api');
 const config = require('config');
 const { findBySlackUserId } = require('../users');
 const logger = require('../../logger');
+const GitSlackinError = require('../../GitSlackinError');
 
 // Setup Slack web client
 const token = config.get('slack');
@@ -44,7 +45,12 @@ function sendMessage(conversationId, message) {
       return res;
     })
     .catch(e => {
-      logger.error(`[slack.message.sendMessage] Error '${e.code}': ${e.data.error}\nDetails:\n${JSON.stringify(e.data)}`);
+      const err = new GitSlackinError(
+        'slack.message.sendMessage',
+        e,
+        `Error '${e.code}': ${e.data.error}. Details: ${JSON.stringify(e.data)}`
+      );
+      err.log();
     });
 }
 
@@ -59,9 +65,8 @@ function sendEphemeralMessage(conversationId, userId, message) {
       return res;
     })
     .catch(e => {
-      logger.error('EPHEMERAL ERROR:');
-      logger.error(e);
-      // throw e;
+      const err = new GitSlackinError('slack.message.sendEphemeralMessage', e);
+      err.log(e);
     });
 }
 
